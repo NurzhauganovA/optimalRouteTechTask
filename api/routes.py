45754a73a903dfd87a3724665a1bcccf87830d98
NurzhauganovA <2003anvarbek2003@gmail.com>
2024-04-03 17:34:14 +0500

    fixes: route API

--- api/routes.py
+++ api/routes.py
@@ -9,13 +9,15 @@ route_router = APIRouter()
 route_repository = RouteRepository(db=get_db())
 
 
-@route_router.post("/routes", response_model=Route)
+@route_router.post("/api/routes", response_model=Route)
 def create_route(route_data: Route):
-    created_route = route_repository.create_route(route_data.dict())
+    file_path = "route.csv"
+
+    created_route = route_repository.add_routes_to_db_from_csv(file_path)
     return created_route
 
 
-@route_router.get("/routes/{route_id}", response_model=Route)
+@route_router.get("/api/routes/{route_id}", response_model=Route)
 def get_route(route_id: int):
     route_path = RouteService(route_repository).get_optimal_route(route_id)
     return route_path
